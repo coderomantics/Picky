355ae533006a36d098a484d6a2a53cbe7a6d9227
Sau Ching Loke <loke.sau.ching@gmail.com>
2022-07-30 15:27:44 +0100

    feat: pie chart to show results

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,12 +1,43 @@
 
-import {React, useState} from "react";
+import {React, useEffect, useState} from "react";
 import { ApiClient } from "../apiClient";
+import {Chart as ChartJS, ArcElement, Tooltip, Legend} from 'chart.js';
+import {Doughnut} from 'react-chartjs-2';
 
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 
 
 
 export default function Form({title, questions, players}) {
+  
+  const data = {
+    labels: players,
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [1,0],    /*how to set data according to number of players*/
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
   const [score, setScore] = useState({});
   const api = new ApiClient();
 
@@ -43,16 +74,17 @@ export default function Form({title, questions, players}) {
           {players.map((p) =>
         
           <label>
-            <input
-              type='radio'
-              name={qn}
-              value={p}
-              onClick={answerHandler}
-              required/>
+              <input
+                type='radio'
+                name={qn}
+                value={p}
+                onClick={answerHandler}
+                required />
               {p}
-          </label>
+            </label>
      
           )}   
+          <Doughnut data={data} />
       
           
 
