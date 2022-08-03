76e33912e94479ae8771716f7244cbcd74cd6d94
Sau Ching Loke <loke.sau.ching@gmail.com>
2022-08-03 10:59:18 +0100

    refactor dahsboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
 import {Link} from 'react-router-dom';
-import {GrAddCircle} from 'react-icons/gr';
-
 import '../css/Dashboard.css';
 import { SocketProvider } from '../App';
 
@@ -33,32 +31,26 @@ export default function Dashboard({title}) {
   return (
   
     <div id='game-header'>
-    
-      {/* <GrAddCircle id='add-btn'/> */}
       <div className='pics'>
-      <img id='eyeball' src='/eyeball2.png' alt='img'/>
-      <Link style={{height: 'fit-content'}} to='/players'>
-        <img id='new-game-btn'
-          // src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/555429/donut.svg'
-          src='/donut.png'
-          alt='+'
-        /> 
-      </Link> 
-       <img id='mushroom' src='/enoki.png' alt='img'/>
-       <img id='cactus' src='/cactus.png' alt='img'/>
-       <img id='gem' src='/gem.png' alt='img'/>
-       <img id='bears' src='/gummybears.png' alt='img'/>
-       <Link style={{textDecoration: 'none', color: 'pink', height: 'fit-content'}} to='/form'>
-        <img id='my-game-btn'
-          src='/heart.png'
-          alt={title}
-        />
-       </Link>
-     
-       
+          <img id='eyeball' src='/eyeball2.png' alt='img'/>
+          <Link style={{height: 'fit-content'}} to='/players'>
+            <img id='new-game-btn'
+              // src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/555429/donut.svg'
+              src='/donut.png'
+              alt='+'
+            /> 
+          </Link> 
+          <img id='mushroom' src='/enoki.png' alt='img'/>
+          <img id='cactus' src='/cactus.png' alt='img'/>
+          <img id='gem' src='/gem.png' alt='img'/>
+          <img id='bears' src='/gummybears.png' alt='img'/>
+          <Link style={{textDecoration: 'none', color: 'pink', height: 'fit-content'}} to='/form'>
+            <img id='my-game-btn'
+              src='/heart.png'
+              alt={title}
+            />
+          </Link>
       </div>
-      
-      {/* <Link style={{textDecoration: 'none', color: 'pink', height: 'fit-content'}} to='/form'>{title}</Link> */}
       <p style={{height: 'fit-content'}}>Connected: {'' + isConnected}</p>
     </div>
     
