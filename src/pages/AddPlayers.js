25d22dafcf952b7c1e7c903977c06f3d7abb6ea6
Sau Ching Loke <loke.sau.ching@gmail.com>
2022-08-03 10:56:09 +0100

    refactor addplayers.js

--- src/pages/AddPlayers.js
+++ src/pages/AddPlayers.js
@@ -24,33 +24,28 @@ export default function CreateGame({ players, addPlayer, removePlayer }) {
     <div className='add-players-container'>
       <p id='create-game'>Create New Game</p>
       <form onSubmit={addHandler} className='form-container'>
-      <input id='input-box'
-        type='text'
-        name='name'
-        onChange={(e) => setInputText(e.target.value)}
-        value={inputText}
-        required/>
-      <div id='add-btn'>
-      <button id='add-players-button' type='submit'> <p>Add player</p></button>
-      </div>
+        <input id='input-box'
+          type='text'
+          name='name'
+          onChange={(e) => setInputText(e.target.value)}
+          value={inputText}
+          required/>
+        <div id='add-btn'>
+          <button id='add-players-button' type='submit'> <p>Add player</p></button>
+        </div>
       </form>
       <div className='players-list-container'>
-      {players.map((p, index) => 
-      <li className='players-list' key={index}>
-        <p className='player-name'>{p.name}</p>
-        <button className='remove-button' onClick={() => removePlayer(index)}> X </button>
-      </li>)}
-     
+        {players.map((p, index) => 
+            <li className='players-list' key={index}>
+              <p className='player-name'>{p.name}</p>
+              <button className='remove-button' onClick={() => removePlayer(index)}> X </button>
+            </li>)}
+    
       </div>
       
       <div className='new-game'>
         <Link style={{textDecoration: 'none'}} to='/questions'><p className='new-game-btn'>New Game</p> </Link>
       </div>
-
-      
-    
-
-
 
     </div>
     </>
