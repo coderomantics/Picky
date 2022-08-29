8a3b4daee57a19aa266a39c5bce0055f6391003b
Sau Ching Loke <loke.sau.ching@gmail.com>
2022-08-29 15:51:09 +0100

    refactor: fixed css and added descriptions

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import {Link} from 'react-router-dom';
 import '../css/Dashboard.css';
 import { SocketProvider } from '../App';
+import Tippy from '@tippyjs/react';
 
 
 
@@ -34,22 +35,35 @@ export default function Dashboard({title}) {
       <div className='pics'>
           <img id='eyeball' src='/eyeball2.png' alt='img'/>
           <Link style={{height: 'fit-content'}} to='/players'>
-            <img id='new-game-btn'
-              // src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/555429/donut.svg'
-              src='/donut.png'
-              alt='+'
-            /> 
+            <Tippy content='Create new game' placement='top'>
+              <img id='new-game-btn'
+                // src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/555429/donut.svg'
+                src='/donut.png'
+                alt='+'
+              /> 
+            </Tippy>
+            
           </Link> 
           <img id='mushroom' src='/enoki.png' alt='img'/>
-          <img id='cactus' src='/cactus.png' alt='img'/>
+       
+            <img id='cactus' src='/cactus.png' alt='img'/>
+
+       
+          
           <img id='gem' src='/gem.png' alt='img'/>
           <img id='bears' src='/gummybears.png' alt='img'/>
-          <Link style={{textDecoration: 'none', color: 'pink', height: 'fit-content'}} to='/form'>
-            <img id='my-game-btn'
+          <div className='link'>
+          
+            <Link style={{height: 'fit-content'}} to='/form'>  
+            <Tippy content="Go to game" theme='light' >
+              <img id='my-game-btn'
               src='/heart.png'
               alt={title}
-            />
-          </Link>
+              />
+            </Tippy>
+            </Link>
+            
+          </div>        
       </div>
       <p style={{height: 'fit-content'}}>Connected: {'' + isConnected}</p>
     </div>
